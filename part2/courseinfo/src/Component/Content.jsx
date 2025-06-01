68c7ae0a6a634ac3c09893c374f772367453fb26
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-06-02 01:46:09 +0600

    2.3*: Course information step 8 (calculate the sum of exercises with the array method reduce)

--- part2/courseinfo/src/Component/Content.jsx
+++ part2/courseinfo/src/Component/Content.jsx
@@ -1,11 +1,12 @@
 import Part from "./Part"
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
     return (
         <div>
-            <Part part={parts[0]} />
-            <Part part={parts[1]} />
-            <Part part={parts[2]} />
+            {parts.map((part) => (
+                <Part key={part.id} part={part} />
+            )
+            )}
         </div>
     )
 }
