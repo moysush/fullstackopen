3806164b04250425df1b64f7d742e3614c955824
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-06-02 01:09:05 +0600

    2.1: Course information step 6 (break down the components)

---
+++ part2/courseinfo/src/Component/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part"
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            <Part part={parts[0]} />
+            <Part part={parts[1]} />
+            <Part part={parts[2]} />
+        </div>
+    )
+}
+
+export default Content
