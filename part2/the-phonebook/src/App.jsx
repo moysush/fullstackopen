4ab188d80ac85adb653ea14ee60567059163606c
isushmoy <hiraku.kazuma5@gmail.com>
2025-06-03 12:33:19 +0600

    2.9*: The Phonebook Step 4 (Search field to filter with partial name)

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-4567890' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -23,12 +27,25 @@ const App = () => {
     setNewName("")
     setNewNum("")
   }
+
+  const handleFilter = (e) => {
+    const value = e.target.value;
+    setFilter(value)
+  }
+
+  const personsToShow = filter ? 
+  persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  : persons
+  console.log(personsToShow);
+  
   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter: <input value={filter} onChange={handleFilter}/></div>
       <form>
+        <h2>Add a new contact</h2>
         <div>
           Name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
@@ -36,13 +53,13 @@ const App = () => {
           Number: <input value={newNum} onChange={(e) => setNewNum(e.target.value)}/>
         </div>
         <div>
-          <button type="submit" onClick={handleSubmit}>add</button>
+          <button type="submit" onClick={handleSubmit}>Add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Number list</h2>
       <div>
-        {persons.map(person => 
-          <p key={person.name}>{person.name}</p>
+        {personsToShow.map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
