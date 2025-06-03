e3969e447ff521d695b75fa514c9f42851941e1a
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-06-03 12:50:08 +0600

    2.10: The Phonebook Step 5 (extract components for reusability)

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -1,12 +1,44 @@
 import { useState } from 'react'
 
+const Filter = ({ filter, handleFilter }) => {
+  return (
+    <div>Filter: <input value={filter} onChange={handleFilter} /></div>
+  )
+}
+
+const PersonForm = ({ newName, setNewName, newNum, setNewNum, handleSubmit }) => {
+  return (
+    <form>
+      <div>
+        Name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>
+        Number: <input value={newNum} onChange={(e) => setNewNum(e.target.value)} />
+      </div>
+      <div>
+        <button type="submit" onClick={handleSubmit}>Add</button>
+      </div>
+    </form>
+  )
+}
+
+const PersonsList = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map(person =>
+        <p key={person.name}>{person.name} {person.number}</p>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filter, setFilter] = useState('')
@@ -17,8 +49,8 @@ const App = () => {
       name: newName,
       number: newNum
     }
-    
-    if(persons.some(person => person.name === newName)){
+
+    if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return // wont execute the codes below if true
     }
@@ -33,35 +65,18 @@ const App = () => {
     setFilter(value)
   }
 
-  const personsToShow = filter ? 
-  persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-  : persons
-  console.log(personsToShow);
-  
-  
+  const personsToShow = filter ?
+    persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>Filter: <input value={filter} onChange={handleFilter}/></div>
-      <form>
-        <h2>Add a new contact</h2>
-        <div>
-          Name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
-        </div>
-        <div>
-          Number: <input value={newNum} onChange={(e) => setNewNum(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" onClick={handleSubmit}>Add</button>
-        </div>
-      </form>
-      <h2>Number list</h2>
-      <div>
-        {personsToShow.map(person => 
-          <p key={person.name}>{person.name} {person.number}</p>
-        )}
-      </div>
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <h3>Add a new contact</h3>
+      <PersonForm newName={newName} setNewName={setNewName} newNum={newNum} setNewNum={setNewNum} handleSubmit={handleSubmit} />
+      <h3>Number list</h3>
+      <PersonsList personsToShow={personsToShow} />
     </div>
   )
 }
