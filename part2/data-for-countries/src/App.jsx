4990c1a93e9327a34bd8b994debcfd37fcecb957
isushmoy <hiraku.kazuma5@gmail.com>
2025-06-14 02:16:28 +0600

    show more details for one country conditionally

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -20,20 +20,36 @@ function App() {
 
   return (
     <>
-      <span>Find countries: </span>
+      <span>Search countries: </span>
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
       <div>
         <br />
         { // only check the next condition if the user starts typing
-        search.length < 1 ? 'Start typing to search...'
-          : // don't show country list if there are more than 10 matches
-          filteredCountries.length > 10 ?
-            'Too many matches, specify another filter'
-            : // list the names from the filtered array
-            filteredCountries.map(country => (
-              <p key={country.cca2}>{country.name.common}</p>
-            )
-            )
+          search.length < 1 ? 'Start typing to search...'
+            : // don't show country list if there are more than 10 matches
+            filteredCountries.length === 1 ?
+              <div>
+                <h1>{filteredCountries[0].name.common}</h1>
+                <p>Capital: {filteredCountries[0].capital}</p>
+                <p>Area: {filteredCountries[0].area}</p>
+                <p>Population: {filteredCountries[0].population}</p>
+                <h4>Timezones: </h4>
+                <ul>
+                  {Object.values(filteredCountries[0].timezones).map(zone => <li key={zone}>{zone}</li>)}</ul>
+                <h2>Languages:</h2>
+                <ul>
+                  {Object.values(filteredCountries[0].languages).map(lang => <li key={lang}>{lang}</li>)}
+                </ul>
+                <img src={filteredCountries[0].flags.png} style={{width: '250px'}}/>
+              </div>
+              :
+              filteredCountries.length > 10 ?
+                'Too many matches, specify another filter'
+                : // list the names from the filtered array
+                filteredCountries.map(country => (
+                  <p key={country.cca3}>{country.name.common}</p>
+                )
+                )
         }
       </div>
     </>
