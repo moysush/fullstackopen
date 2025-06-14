32f41ed42ccf20f4ec4fa26f3901af09b912cda4
isushmoy <hiraku.kazuma5@gmail.com>
2025-06-14 22:48:26 +0600

    2.19*: Data for countries, step 2 (show button to view information of that coutnry; achieved it just using the setCountry state on the button!)

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -2,6 +2,25 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
+const DetailedCountry = ({ filteredCountries }) => {
+  return (
+    <div>
+      <h1>{filteredCountries[0].name.common}</h1>
+      <p>Capital: {filteredCountries[0].capital}</p>
+      <p>Area: {filteredCountries[0].area}</p>
+      <p>Population: {filteredCountries[0].population}</p>
+      <h4>Timezones: </h4>
+      <ul>
+        {Object.values(filteredCountries[0].timezones).map(zone => <li key={zone}>{zone}</li>)}</ul>
+      <h2>Languages:</h2>
+      <ul>
+        {Object.values(filteredCountries[0].languages).map(lang => <li key={lang}>{lang}</li>)}
+      </ul>
+      <img src={filteredCountries[0].flags.png} style={{ width: '250px' }} />
+    </div>
+  )
+}
+
 function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
@@ -28,26 +47,15 @@ function App() {
           search.length < 1 ? 'Start typing to search...'
             : // don't show country list if there are more than 10 matches
             filteredCountries.length === 1 ?
-              <div>
-                <h1>{filteredCountries[0].name.common}</h1>
-                <p>Capital: {filteredCountries[0].capital}</p>
-                <p>Area: {filteredCountries[0].area}</p>
-                <p>Population: {filteredCountries[0].population}</p>
-                <h4>Timezones: </h4>
-                <ul>
-                  {Object.values(filteredCountries[0].timezones).map(zone => <li key={zone}>{zone}</li>)}</ul>
-                <h2>Languages:</h2>
-                <ul>
-                  {Object.values(filteredCountries[0].languages).map(lang => <li key={lang}>{lang}</li>)}
-                </ul>
-                <img src={filteredCountries[0].flags.png} style={{width: '250px'}}/>
-              </div>
+              <DetailedCountry filteredCountries={filteredCountries} />
               :
               filteredCountries.length > 10 ?
                 'Too many matches, specify another filter'
                 : // list the names from the filtered array
                 filteredCountries.map(country => (
-                  <p key={country.cca3}>{country.name.common}</p>
+                  <>
+                    <p key={country.cca3}>{country.name.common} <button onClick={() => setSearch(country.name.common)}>Show</button></p>
+                  </>
                 )
                 )
         }
