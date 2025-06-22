aae321f24509ccebfcc60769a88aad2f3fd278c1
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-06-22 16:45:49 +0600

    3.2: Phonebook backend step 2 (info page with persons list length and the time, the request was made at )

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,21 +7,21 @@ const persons = [
         "name": "Arto Hellas",
         "number": "040-123456"
     },
-{
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-},
-{
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-},
-{
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-}
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
 ]
 
 app.get('/', (req, res) => {
@@ -30,6 +30,12 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.send(persons)
+})
+
+// info page
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> 
+        <p>${Date()}</p>`)
 })
 
 const PORT = 3001
