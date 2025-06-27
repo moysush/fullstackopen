39d4d17bc2005a11258ac4432f6808e0dc7427c8
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-06-28 01:09:32 +0600

    3.8*: Phonebook backend step 8 (show the data from req using a new body token of morgan)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,8 +3,10 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json()) // for recieving data with json.parser
- 
-app.use(morgan('tiny'))
+
+// create token body to get the body content and stringify it show it as a string
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
