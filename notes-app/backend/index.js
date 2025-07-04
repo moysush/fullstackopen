4197bad8190e425eb66866ce2a3c03d9a38e6f30
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-07-04 23:29:28 +0600

    update css

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -3,10 +3,11 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+// to use the frontend, we need to use the dist build directory with the static middleware
 app.use(express.static('dist'))
 
-let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }]
+let notes = [{ id: "1", content: "HTML is easy", important: true }, { id: "2", content: "Browser can execute only JavaScript", important: false }, { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1> ')
@@ -17,13 +18,13 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if(note){
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    if (note) {
         response.json(note)
-} else {
-    response.status(404).end()
-}
+    } else {
+        response.status(404).end()
+    }
 
 })
 
