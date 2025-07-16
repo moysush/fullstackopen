1d49004b1def139605ac25d642d68ecfe9da2196
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-07-16 18:46:18 +0600

    3.12: Command-line database (add mongodb file to test saving data in the mongodb database cloud through mongo atlas)

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+// arguments
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://sushmoy:${password}@cluster0.ipwbpuv.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(url)
+
+const personSchema = mongoose.Schema({
+    Name: String,
+    Number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    Name: name,
+    Number: number
+})
+
+
+if (process.argv.length < 4) {
+    // using Person directly because we have no use of person object here + it will only give us error
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person);
+        })
+        mongoose.connection.close()
+    })
+} else {
+    // saving the person
+    person.save().then(result => {
+        console.log(`added ${name}, number ${number} to the phonebook`)
+        mongoose.connection.close()
+    })
+}
