f13c68c4d3a61060b159272ab6439566671df0ba
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-01 19:27:33 +0600

    update and delete notes from the mongodb using findById and findByIdAndDelete

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -5,9 +5,28 @@ const app = express()
 // loading note module
 const Note = require('./models/note')
 
+// request logger
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+// error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
 // to use the frontend, we need to use the dist build directory with the static middleware
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
 // app.use(cors())
 
 // let notes = [{ id: "1", content: "HTML is easy", important: true }, { id: "2", content: "Browser can execute only JavaScript", important: false }, { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }]
@@ -46,8 +65,11 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
+    // notes = notes.filter(note => note.id !== id)
+    Note.findByIdAndDelete(id).then(result => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // const generateId = () => {
@@ -80,6 +102,23 @@ app.post('/api/notes', (req, res) => {
         .then(savedNote => res.json(savedNote)) // will only show in req or res netowrk section
 })
 
+app.put('/api/notes/:id', (req, res, next) => {
+    const {content, important} = req.body
+
+    Note.findById(req.params.id)
+        .then(note => {
+            if(!note){
+                return res.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save().then(updatedNote => res.json(updatedNote))
+        })
+        .catch(error => next(error))
+})
+
 
 // handler of requests with unknown endpoint
 const unknownEndpoint = (request, response) => {
@@ -87,16 +126,6 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
-// error handler middleware
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    next(error)
-}
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT
