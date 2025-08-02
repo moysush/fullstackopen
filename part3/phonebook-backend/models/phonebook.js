6ea802755468a22477cbfe7fbcfaab57d001e57c
sush <hiraku.kazuma5@gmail.com>
2025-08-02 23:08:46 +0600

    3.21 Deploying the database backend to production

--- part3/phonebook-backend/models/phonebook.js
+++ part3/phonebook-backend/models/phonebook.js
@@ -1,17 +1,7 @@
 // mongodb
 const mongoose = require('mongoose')
 
-// if(process.argv.length < 3){
-//     console.log('Give password as an argument');
-//     process.exit(1)
-// }
-
 mongoose.set('strictQuery', false)
-
-// arguments
-// const password = process.argv[2]
-// const name = process.argv[3]
-// const number = process.argv[4]
 
 const url = process.env.MONGODB_URL
 
