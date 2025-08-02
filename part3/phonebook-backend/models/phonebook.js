d4be2b127dacb0c0aeb76d966236c3d0f649089a
sush <hiraku.kazuma5@gmail.com>
2025-08-02 23:07:08 +0600

    3.20*: Phonebook database, step 8, number validation

--- part3/phonebook-backend/models/phonebook.js
+++ part3/phonebook-backend/models/phonebook.js
@@ -21,8 +21,25 @@ const personSchema = mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
+        required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function(v) {
+                // Regex pattern: 
+                // ^: start of string
+                // \d{2,3}: 2 or 3 digits
+                // -: literal dash
+                // \d+: one or more digits
+                // $: end of string
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number! Format should be XX-XXXXXX or XXX-XXXXXX`
+        }
+    }
 })
 
 // remove __v from the json in response
