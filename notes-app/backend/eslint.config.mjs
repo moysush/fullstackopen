1404aca5c75df2148a3c515352e1842ebbfa4eb2
sush <hiraku.kazuma5@gmail.com>
2025-08-06 00:28:42 +0600

    add eslint to the project

---
+++ notes-app/backend/eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'], plugins: { js, '@stylistic': stylistic }, extends: ['js/recommended', 'airbnb'], languageOptions: { globals: globals.node },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
+    },
+    ignores: ['dist/**']
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+])
--- notes-app/backend/models/note.js
+++ notes-app/backend/models/note.js
@@ -8,33 +8,31 @@ const mongoose = require('mongoose')
 
 // const password = process.argv[2]
 
-// const MONGODB_URI = `mongodb+srv://sushmoy:${password}@cluster0.ipwbpuv.mongodb.net/noteapp?retryWrites=true&w=majority&appName=Cluster0`
-
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => console.log('connected'))
-    .catch(error => console.log('an error occured', error.message))
+  .then(() => console.log('connected'))
+  .catch(error => console.log('an error occured', error.message))
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    date: String,
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: String,
+  important: Boolean,
 })
 
 // remove the id and version property of mongo.js
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 // const Note = mongoose.model('Note', noteSchema)
 
