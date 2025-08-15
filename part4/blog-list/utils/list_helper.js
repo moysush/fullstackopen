0cbeb26e67d438f7ef6d7b6a2fe9b6bcd7972194
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-15 12:49:07 +0600

    4.5*: Helper Functions and Unit Tests, step 3, test for a favorite blog using the most likes

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,4 +6,8 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
