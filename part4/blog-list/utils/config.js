2ccc20f53404b45188411e5a25c15173db7749cf
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-20 15:39:11 +0600

    4.8: Blog List Tests, step 1, add test environment, add supertest for easier testing with backend api, add beforeEach, after, and test for returning blogs in JSON

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 PORT = process.env.PORT
-MONGODB_URI = process.env.MONGODB_URI
+MONGODB_URI = process.env.NODE_ENV=='test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 
 module.exports = {PORT, MONGODB_URI}
