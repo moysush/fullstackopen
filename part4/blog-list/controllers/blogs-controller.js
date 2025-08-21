47a43a5d70a4a7f4400368934e4b4ecace370701
sush <hiraku.kazuma5@gmail.com>
2025-08-21 21:12:40 +0600

    4.13 Blog List Expansions, step 1, add dele functionality in the server and for the test

--- part4/blog-list/controllers/blogs-controller.js
+++ part4/blog-list/controllers/blogs-controller.js
@@ -1,16 +1,17 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('express')
 const Blog = require('../models/blog-model')
 
-blogsRouter.get('/', async(request, response) => {
+blogsRouter.get('/', async (request, response) => {
     const res = await Blog.find({})
     response.json(res)
 })
 
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-    
+
     if (!request.body.likes) {
-        blog.likes = 0 
+        blog.likes = 0
     }
 
     if (!request.body.title || !request.body.url) {
@@ -19,6 +20,26 @@ blogsRouter.post('/', async (request, response) => {
 
     const res = await blog.save()
     response.status(201).json(res)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    const blog = await Blog.findById(request.params.id)
+    try {
+        if (blog) {
+            blog.likes = request.body.likes
+            const updatedBlog = await blog.save()
+            response.json(updatedBlog)
+        } else {
+            response.status(404).end()
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = blogsRouter
