0a4669de05e207092d00325e8fd0cc363e3e8640
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-24 11:08:13 +0600

    creating new notes with user id and use Populate

--- notes-app/backend/controllers/notes.js
+++ notes-app/backend/controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note.find({}).populate('user', {username: 1, name: 1})
     response.json(notes)
 })
 
@@ -32,13 +33,26 @@ notesRouter.post('/', async (req, res) => {
       error: 'content missing'
     })
   }
+
+  // finding the user
+  const user = await User.findById(body.userId)
+
+  if(!user){
+    return res.status(400).json({error: 'userId missing or not valid'})
+  }
   // obj for sending response
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    // include the user detail
+    user: user._id
   })
 
   const savedNote = await note.save()
+  // save the notes id in user
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   res.status(201).json(savedNote)
 })
 
