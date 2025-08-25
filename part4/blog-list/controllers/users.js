027da16ce69fa385e10fb3b66bc37095440dff50
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-25 23:28:57 +0600

    4.16*: Blog List Expansion, step 4, add requiremnets for username and password, add tests for invalid users with appropriate error messages

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -10,6 +10,11 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body // User model er vitor use korle mongodb format kora hoy
 
+    // password validation
+    if(!password || password.length < 3){
+        return response.status(400).json({error: 'password must be at least 3 characters long'})
+    }
+
     // encrypted password
     const passwordHash = await bcrypt.hash(password, 10)
 
--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,87 +1,93 @@
 const app = require('../app')
-const Blog = require('../models/blog-model')
+const Blog = require('../models/blog')
 const supertest = require('supertest')
-const { test, beforeEach, after } = require('node:test')
+const { test, beforeEach, after, describe } = require('node:test')
 const assert = require('node:assert/strict')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
-const initialBlog = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    }
-]
 
-const blogsInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(blog => blog.toJSON())
-}
+describe('when there is initally two blogs', () => {
+    
+    beforeEach(async () => {
+        await Blog.deleteMany()
+        // console.log('deleted');
+        await Blog.insertMany(helper.initialBlog)
+        // console.log('saved')
+    })
+    
+    test('blogs are returned in JSON format', async () => {
+        // supertest lets us test with expect
+        const res = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        // console.log(res.body);
+    })
+    
+    test('blogs are returned with an id property, not an _id', async () => {
+        const res = await api.get('/api/blogs')
+        assert(!res.body.includes('_id'))
+    })
+    
+    test('creating new blog post', async () => {
+        const addBlog = {
+            title: "Deep Work",
+            author: "Cal Newport",
+            likes: 100,
+            url: "calnewport.com"
+        }
+    
+        const res = await api
+            .post('/api/blogs')
+            .send(addBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const updatedList = await helper.blogsInDb()
+    
+        // checking the length after adding the blog
+        assert.equal(updatedList.length, helper.initialBlog.length + 1)
+    
+        // verifying the new blog without the id as it is dynamic
+        const { title, author, likes, url } = updatedList[2]
+        assert.deepEqual({ title, author, likes, url }, {
+            title: 'Deep Work',
+            author: 'Cal Newport',
+            likes: 100,
+            url: "calnewport.com"
+        })
+    })
 
-beforeEach(async () => {
-    await Blog.deleteMany()
-    // console.log('deleted');
-    await Blog.insertMany(initialBlog)
-    // console.log('saved')
-})
-
-
-
-test('blogs are returned in JSON format', async () => {
-    // supertest lets us test with expect
-    const res = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    // console.log(res.body);
-})
-
-test('blogs are returned with an id property, not an _id', async () => {
-    const res = await api.get('/api/blogs')
-    assert(!res.body.includes('_id'))
-})
-
-test('creating new blog post', async () => {
-    const addBlog = {
-        title: "Deep Work",
-        author: "Cal Newport",
-        likes: 100,
-        url: "calnewport.com"
-    }
-
-    const res = await api
-        .post('/api/blogs')
-        .send(addBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const updatedList = await blogsInDb()
-
-    // checking the length after adding the blog
-    assert.equal(updatedList.length, initialBlog.length + 1)
-
-    // verifying the new blog without the id as it is dynamic
-    const { title, author, likes, url } = updatedList[2]
-    assert.deepEqual({ title, author, likes, url }, {
-        title: 'Deep Work',
-        author: 'Cal Newport',
-        likes: 100,
-        url: "calnewport.com"
+    test('delete a single blog', async () => {
+        const blogs = await helper.blogsInDb()
+        const blogToDelete = blogs[0].id
+    
+        await api
+            .delete(`/api/blogs/${blogToDelete}`)
+            .expect(204)
+    
+        const blogsAfter = await helper.blogsInDb()
+    
+        assert.equal(blogsAfter.length, helper.initialBlog.length - 1)
+    })
+    
+    test('updating individual blog', async () => {
+        const blogToUpdate = helper.initialBlog[0]._id
+        const updatedBlog = {
+            likes: 15
+        }
+        const res = await api
+            .put(`/api/blogs/${blogToUpdate}`) // params
+            .send(updatedBlog) // body
+            .expect(200)
+    
+        assert.equal(res.body.likes, 15)
     })
 })
+
 
 test('set likes: 0 if likes property is missing', async () => {
     const newBlog = {
@@ -111,31 +117,6 @@ test('either title or url deos not exist', async () => {
     // assert.equal(res.status, 400)
 })
 
-test('delete a single blog', async () => {
-    const blogs = await blogsInDb()
-    const blogToDelete = blogs[0].id
-
-    await api
-        .delete(`/api/blogs/${blogToDelete}`)
-        .expect(204)
-
-    const blogsAfter = await blogsInDb()
-
-    assert.equal(blogsAfter.length, initialBlog.length - 1)
-})
-
-test('updating individual blog', async () => {
-    const blogToUpdate = initialBlog[0]._id
-    const updatedBlog = {
-        likes: 15
-    }
-    const res = await api
-        .put(`/api/blogs/${blogToUpdate}`) // params
-        .send(updatedBlog) // body
-        .expect(200)
-
-    assert.equal(res.body.likes, 15)
-})
 
 // if we don't close the db then it will not stop finishing the execution of the test
 after(async () => {
