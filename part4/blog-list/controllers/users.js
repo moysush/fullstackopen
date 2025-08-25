311539fc32c93808c78d4ab567b17d40dfbaac60
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-25 13:16:19 +0600

    4.15: Blog List Expansion, step 3, add user model, add users controller for sending the POST request, use bcrypt for password, save the datas on db

---
+++ part4/blog-list/controllers/users.js
@@ -0,0 +1,27 @@
+const User = require('../models/user')
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body // User model er vitor use korle mongodb format kora hoy
+
+    // encrypted password
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    // assining the values
+    const user = new User({
+        username, 
+        name, 
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
