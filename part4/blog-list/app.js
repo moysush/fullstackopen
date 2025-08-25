687f03fd758b891bd4aef08ffb37bce48764d655
sush <hiraku.kazuma5@gmail.com>
2025-08-25 13:16:19 +0600

    4.15: Blog List Expansion, step 3, add user model, add users controller for sending the POST request, use bcrypt for password, save the datas on db

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const { MONGODB_URI } = require('./utils/config')
 const { info, error } = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs-controller')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -12,5 +13,6 @@ mongoose.connect(MONGODB_URI)
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
