0c75b97f61f22bcc9d75110afcd069fcba4e6f69
sush <hiraku.kazuma5@gmail.com>
2025-08-28 13:41:59 +0600

    Add login form in the frontend, Conditional Rendering of the Login Form

--- notes-app/frontend/src/App.jsx
+++ notes-app/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
-import noteService from './Services/notes'
 import Note from './components/Note'
+import { LoginForm } from './components/Login'
 import Footer from './components/Footer'
+import noteService from './Services/notes'
+import loginService from './Services/login'
 
-const Notification = ({message}) => {
+const Notification = ({ message }) => {
   if (message === null) {
     return null
   }
@@ -19,34 +21,53 @@ const App = () => {
   const [newNote, setNewNote] = useState("Write something...")
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
   useEffect(() => {
     noteService
-    .getAll()
-    .then(initialNote => {
-      setNotes(initialNote)      
-    })
+      .getAll()
+      .then(initialNote => {
+        setNotes(initialNote)
+      })
   }, [])
   console.log('render', notes.length, 'notes');
-  
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)    
-    const changedNote = {...note, important: !note.important} // only the specific note
-    
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-      setNotes(notes.map(note => note.id === id ? returnedNote : note))
-    })
-      .catch(error => {
-      setErrorMessage(`note '${note.content} was already removed from server`)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
+    }
   }
-  
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important } // only the specific note
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
+      .catch(() => {
+        setErrorMessage(`note '${note.content} was already removed from server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
   const addNote = (event) => {
     event.preventDefault()
 
@@ -66,23 +87,34 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={(e) => setNewNote(e.target.value)} />
+      <button type='submit'>Save</button>
+    </form>
+  )
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {!user && <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      )}
+
       <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? 'important' : 'all'}
       </button>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}  />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={(e) => setNewNote(e.target.value)}/>
-        <button type='submit'>Save</button>
-      </form>
       <Footer />
     </div>
   )
