4ead64e349cd58b36e10f5c878afe31616720757
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-28 13:41:59 +0600

    Add login form in the frontend, Conditional Rendering of the Login Form

---
+++ notes-app/frontend/src/components/Login.jsx
@@ -0,0 +1,30 @@
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => (
+  <div id="login">
+    <h2>Login</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type='text'
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+)
+
+export { LoginForm }
