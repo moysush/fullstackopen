e3ec7979eebd028058db6e2d5bef9bf760ffe8e1
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-08-28 20:41:03 +0600

    5.2: Blog List Frontend, step 2, implement localStorage to save user token, add logout functionality, add error state and show it to the user

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,11 +8,19 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      setUser(JSON.parse(loggedUserJSON))
+    }
   }, [])
 
   const handleSubmit = async (e) => {
@@ -23,10 +31,17 @@ const App = () => {
       password: password
     }
 
-    const userData = await loginService.login(newUser)
-    setUser(userData)
-    setUsername('')
-    setPassword('')
+    try {
+      const userData = await loginService.login(newUser)
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(userData))
+      setUser(userData)
+      setUsername('')
+      setPassword('')
+      setError(null)
+    }
+    catch (err) {
+      setError('invalid username or password', err)
+    }
   }
 
   const loginForm = () => (
@@ -51,12 +66,14 @@ const App = () => {
 
   return (
     <div>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+
       {!user && loginForm()}
 
       {user &&
         (<div id='blogs'>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in <button onClick={() => setUser(window.localStorage.removeItem('loggedBlogappUser'))}>logout</button></p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
