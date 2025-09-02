0a22ca72c747b6633f9e9b39a7539318bde708fa
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-09-02 18:43:58 +0600

    5.7 Blog List Frontend, step 7, add a view button so that each blog can be viewed in detail when clicked

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,9 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    <li>
-    {blog.title} - {blog.author}
-    </li>
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+
+  return (
+    <div className="blog">
+      <div style={{ fontWeight: 'bold' }}>
+        {blog.title} <button onClick={() => setView(!view)}>{view ? 'Hide' : 'View'}</button>
+      </div>
+      {
+        view
+          ?
+          <div>
+            <div>
+              {blog.url}
+            </div>
+            <div>
+              Likes: {blog.likes} <button>Like</button>
+            </div>
+            <div>
+              {blog.author}
+            </div>
+          </div>
+          : null
+      }
+    </div>
+  )
+}
 export default Blog
