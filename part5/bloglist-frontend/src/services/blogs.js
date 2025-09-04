16792d239a743a0981193b449b3d22b24dd36ba6
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-09-04 10:00:09 +0600

    5.8: Blog List Frontend, step 8, update likes using the like button, ensure only authorized and the same user who added the blog can update it

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,17 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+const config = (token) => {
+  return {
+    headers: {Authorization: `Bearer ${token}`}
+  }
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
 const create = async(newObj, token) => {
-  const config = {
-    headers: { Authorization: `Bearer ${token}`}
-  }
-  const response = await axios.post(baseUrl, newObj, config)
+  const response = await axios.post(baseUrl, newObj, config(token))
   return response.data
 }
 
-export default { getAll, create }
+const update = async(newObj, token) => {
+  const response = await axios.put(`${baseUrl}/${newObj.id}`, newObj, config(token))
+  return response.data
+}
+
+export default { getAll, create, update }
