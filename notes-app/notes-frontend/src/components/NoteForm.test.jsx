706c6b0cdd6b06b1484f8ebd82ee430aa0e1638f
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-09-23 12:40:23 +0600

    Add placeholder to input field in NoteForm component and create NoteForm test utilizing screee.getByPlaceholderText() for the input and getByText for the button, userEvent.setup() is used for initiating a user session and vi.fn creates a mock function which let us assert it

---
+++ notes-app/notes-frontend/src/components/NoteForm.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import NoteForm from './NoteForm'
+import userEvent from '@testing-library/user-event'
+import { expect } from 'vitest'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+    const createNote = vi.fn()
+    const user = userEvent.setup()
+
+    render(<NoteForm createNote={createNote} />)
+
+    const input = screen.getByPlaceholderText('content')
+    const sendButton = screen.getByText('Save')
+
+    await user.type(input, 'testing a form...')
+    await user.click(sendButton)
+
+    expect(createNote.mock.calls).toHaveLength(1)
+    expect(createNote.mock.calls[0][0].content).toBe('Write something...testing a form...')
+
+    console.log(createNote.mock.calls)
+})
