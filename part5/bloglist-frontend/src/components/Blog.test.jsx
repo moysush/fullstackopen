a7d590de4b0ce990054deb647a73120063330859
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-09-23 20:28:50 +0600

    5.13: Blog List Tests, step 1; add vitest, jsdom, and @testing-library/react; configure vitest for the project with jsdom in vite.config.js and linked it to testSetup.js from the root, add a test for blog, the test checks if the blog renders correctly with the author and title excluding url and likes by default

---
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import { expect, test } from "vitest"
+import Blog from "./Blog"
+
+test('blog renders the title and author', () => {
+    const blog = {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+    }
+
+    render(<Blog blog={blog} />)
+
+    const titleAndAuthor = screen.getByText(/React patterns/)
+    const url = screen.queryByText("https://reactpatterns.com/")
+    const likes = screen.queryByText(/Likes: 7/)
+
+    // expect(titleAndAuthor).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+})
