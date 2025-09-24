5c5fee73f690954524d2a7c89f74bffce033279a
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-09-24 10:52:06 +0600

    5.15: Blog List Tests, step 3, button is clicked twice checked using mockHandler calls

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,10 +1,12 @@
 import { fireEvent, render, screen } from "@testing-library/react"
-import { beforeEach, describe, expect, test } from "vitest"
+import { beforeEach, describe, expect, test, vi } from "vitest"
 import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
 
 describe('blogs', () => {
+    let mockHandler
     beforeEach(() => {
+        mockHandler = vi.fn()
         const blog = {
             title: "React patterns",
             author: "Michael Chan",
@@ -14,7 +16,7 @@ describe('blogs', () => {
                 name: "test user"
             }
         }
-        render(<Blog blog={blog} />)
+        render(<Blog blog={blog} updatelikes={mockHandler} />)
 
     })
     test('blog renders the title and author', () => {
@@ -28,12 +30,23 @@ describe('blogs', () => {
         expect(likes).toBeNull()
     })
 
-    test("blog's URL and number of likes are shown when the button is clicked", async () => {
+    describe("blog's view button", () => {
         const user = userEvent.setup()
-        await user.click(screen.getByText("View"))
 
-        expect(screen.getByText("https://reactpatterns.com/")).toBeDefined();
-        screen.getByText(/Likes: 7/)
+        test("blog's URL and number of likes are shown when the button is clicked", async () => {
+            await user.click(screen.getByText("View"))
+            expect(screen.getByText("https://reactpatterns.com/")).toBeDefined();
+            screen.getByText(/Likes: 7/)
+        })
+
+        test("like button clicked twice", async () => {
+            await user.click(screen.getByText("View"))
+            // clicking twice
+            await user.click(screen.getByText("Like"))
+            await user.click(screen.getByText("Like"))
+            // checking the function handler using mockHandler calls length
+            expect(mockHandler.mock.calls.length).toBe(2)
+        })
     })
 
 })
