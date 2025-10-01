7980c73ecfea5bf954dccd118421929fddcbfba0
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-01 14:03:31 +0600

    5.19: Blog List End To End Testing, step 3, logged in user can create new blog

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 import { loginUser } from './helper';
+import { beforeEach } from 'node:test';
 
 test.describe('Blog App', () => {
   test.beforeEach(async ({ page, request }) => {
@@ -34,6 +35,22 @@ test.describe('Blog App', () => {
       await loginUser(page, "sush", "wrong")
 
       await expect(page.getByText('invalid username or passwor')).toBeVisible()
+    })
+  })
+
+  test.describe('when logged in', () => {
+    test.beforeEach(async ({ page }) => {
+      await loginUser(page, "sush", "root")
+    })
+    test('a new blog can be created', async({page}) => {
+      await page.getByRole('button', {name: /create new blog/i}).click()
+
+      await page.getByLabel(/title/i).fill('Deep Work')
+      await page.getByLabel(/author/i).fill('Cal Newport')
+      await page.getByLabel(/url/i).fill('https://calnewport.com/deep-work-rules-for-focused-success-in-a-distracted-world/')
+      await page.getByRole('button', {name: /create/i}).click()
+
+      await expect(page.getByText('a new blog, Deep Work by Cal Newport was added successfully')).toBeVisible()
     })
   })
 
