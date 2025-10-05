7df241afa8f7972f72b669808595f4e7fbd103c1
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-05 19:36:48 +0600

    6.2: Unicafe Revisited, step2, connect store with the buttons of the app to implement the functionality

--- part6/unicafe-revisited/src/reducers/reducer.test.js
+++ part6/unicafe-revisited/src/reducers/reducer.test.js
@@ -1,5 +1,5 @@
 import deepFreeze from 'deep-freeze'
-import counterReducer from './counterReducer'
+import counterReducer from './reducer'
 
 describe('unicafe reducer', () => {
   const initialState = {
