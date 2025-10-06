7a7b2a74c94794174f4b0309825142308ed9a5e3
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-06 18:57:42 +0600

    Forwarding Redux Store to various components using react-redux library

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer.js'
+
+const store = createStore(noteReducer)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 )
