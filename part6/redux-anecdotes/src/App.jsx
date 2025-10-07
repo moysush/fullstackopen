dc17fdf6372000628bb5cd8ade4ba0fcbca3d2ee
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-07 13:20:46 +0600

    6.8: Anecdotes, step 6, separate AnecdoteList and AnecdoteForm into their own component for cleaner code

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,40 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, vote } from './reducers/anecdoteReducer'
+import { AnecdoteList } from './components/AnecdoteList.jsx'
+import { AnecdoteForm } from './components/AnecdoteForm.jsx'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
-    event.preventDefault() // prevent reload
-    const content = event.target.input.value
-    event.target.input.value = '' // emptying th event field for user
-
-    dispatch(createAnecdote(content))
-  }
-
+// much cleaner withthe components moved to their own component
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .slice()
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="input" /></div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
