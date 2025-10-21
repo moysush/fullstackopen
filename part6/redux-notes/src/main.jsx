1543a797585cdc7d2e3231ef4b5519265de25100
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-21 19:35:24 +0600

    Redux Toolkit and Refactoring Reducers with createSlice()

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -2,23 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
+
 import App from './App.jsx'
-import { combineReducers, createStore } from 'redux'
-import { noteReducer } from './reducers/noteReducer.js'
-import { filterReducer } from './reducers/filterReducer.js'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
-
-console.log(store.getState());
-
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
-// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+import { store } from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
