9e838eb0409d9235dbfec89954d1c19183b2c9a8
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-10-24 14:40:34 +0600

    make changes in the json-server from the app

--- part6/redux-notes/src/services/notes.js
+++ part6/redux-notes/src/services/notes.js
@@ -9,3 +9,35 @@ export const getAll = async() => {
 
     return  await response.json()
 }
+
+export const createNew = async (content) => {
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({content, important: false})
+    }
+
+    const response = await fetch(baseUrl, options)
+
+    if(!response.ok){
+        throw new Error('Failed to create note')
+    }
+
+    return await response.json()
+}
+
+export const updateNote = async(id, content, important) => {
+    const options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({content, important: !important})
+    }
+
+    const response = await fetch(`${baseUrl}/${id}`, options)
+
+    if(!response.ok){
+        throw new Error('Can not update the note')
+    }
+
+    return await response.json()
+}
