00641ded41d550a4beb0d37398c55a49c9b5979f
moysush <m.hossain.sushmoy@gmail.com>
2025-10-24 19:53:56 +0600

    6.14 Anecdotes and the Backend, step 1, use getAll() service for fetching data from json-server

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,15 +2,27 @@ import { AnecdoteList } from './components/AnecdoteList.jsx'
 import { AnecdoteForm } from './components/AnecdoteForm.jsx'
 import Notification from './components/Notification.jsx'
 import Filter from './components/Filter.jsx'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getAll } from './services/anecdotes.js'
+import { anecdoteSlice } from './reducers/anecdoteReducer.js'
 
-const App = () => /* much cleaner withthe components moved to their own component*/ (
-  <div>
-    <h2>Anecdotes</h2>
-    <Notification />
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+const App = () => {/* much cleaner withthe components moved to their own component*/
 
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    getAll().then(anecdotes => dispatch(anecdoteSlice.actions.setAnecdotes(anecdotes)))
+  }, [dispatch])
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
 export default App
