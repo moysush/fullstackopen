27ea6d6a96cd6d3459b27f07883c1b13f6a15103
moysush <m.hossain.sushmoy@gmail.com>
2025-11-11 09:41:46 +0600

    separate all the code related to CounterContent in its file

--- part6/hook-counter/src/App.jsx
+++ part6/hook-counter/src/App.jsx
@@ -1,33 +1,17 @@
-import { useReducer } from 'react'
 import Display from './components/Display'
 import Button from './components/Button'
-import CounterContext from './components/CounterContext'
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    case 'DEC':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
 
   return (
-    <CounterContext value={{counter, counterDispatch}}>
+    <div>
       <Display />
       <div>
         <Button type={'INC'} label={'+'}/>
         <Button type={'DEC'} label={'-'}/>
         <Button type={'ZERO'} label={'ZERO'}/>
       </div>
-    </CounterContext>
+    </div>
   )
 }
 
