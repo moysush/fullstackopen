8f2738773337d7fc85ef8fedb556e8caa212bb06
Sush <m.hossain.sushmoy@gmail.com>
2025-12-14 22:37:50 +0600

    Add Roboto font

--- part7/Side Projects/react-router-MUI/src/main.jsx
+++ part7/Side Projects/react-router-MUI/src/main.jsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+// fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
