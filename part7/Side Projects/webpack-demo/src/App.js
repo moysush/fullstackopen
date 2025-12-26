654cf80ef3a5db812421a8874c9d983c5cfffea6
Sush <m.hossain.sushmoy@gmail.com>
2025-12-26 23:00:11 +0600

    Development and production configuration

--- part7/Side Projects/webpack-demo/src/App.js
+++ part7/Side Projects/webpack-demo/src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './index.css'
+import axios from "axios";
+const useNotes = (url) => {
+    const [notes, setNotes] = useState([])
+        useEffect(() => {
+        axios.get(url).then(response => {
+            setNotes(response.data)
+        })
+    }, [url])
+    return notes
+}
 
 const App = () => {
     const [counter, setCounter] = useState(0)
     const [value, setValue] = useState([])
+    const notes = useNotes(BACKEND_URL)
 
     const handleCLick = () => {
-        setCounter(counter+10)
+        setCounter(counter + 10)
         setValue(value.concat(counter))
     }
     return (
@@ -16,6 +27,7 @@ const App = () => {
             <button onClick={handleCLick}>
                 press
             </button>
+            <div>{notes.length} notes on server {BACKEND_URL}</div>
             {/* {value} */}
         </div>
     )
