75442a89841daf2b5233ff8c8b9dd2967e8a3919
Sush <m.hossain.sushmoy@gmail.com>
2025-12-26 13:23:29 +0600

    webpack-dev-server

--- part7/Side Projects/webpack-demo/src/App.js
+++ part7/Side Projects/webpack-demo/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import './index.css'
 
 const App = () => {
+    const [counter, setCounter] = useState(0)
     return (
         <div className="container">
-            hello, webpack!
+            hello, webpack {counter} clicks
+            <button onClick={() => setCounter(counter+10)}>
+                press
+            </button>
         </div>
     )
 }
