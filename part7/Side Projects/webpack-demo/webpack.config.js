ca5fff6e9a3a536b63c65cfacbd3cfafa0710c3e
Sushmoy <m.hossain.sushmoy@gmail.com>
2025-12-26 23:00:11 +0600

    Development and production configuration

--- part7/Side Projects/webpack-demo/webpack.config.js
+++ part7/Side Projects/webpack-demo/webpack.config.js
@@ -1,33 +1,55 @@
 const path = require('path');
 const { presets } = require('./babel.config');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    compress: true,
-    port: 3000
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+
+  console.log(argv.mode);
+  
+  const backend_url = isProd ? 'https://notes2023.fly.dev/api/notes'
+    : 'http://localhost:3001/notes';
+
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
+      proxy: [
+        {
+          context: ['/api'],
+          target: 'http://localhost:3001',
+          changeOrigin: true
         }
-      },
-      {
-        test: /\.css/,
-        use: ['style-loader', 'css-loader']
-      }
-    ],
-  },
+      ]
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        {
+          test: /\.css/,
+          use: ['style-loader', 'css-loader']
+        }
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
+  }
 };
