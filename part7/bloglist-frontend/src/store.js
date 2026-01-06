c7ac593c5b77588a82652b9dedb0ee55916bc89f
Sushmoy <m.hossain.sushmoy@gmail.com>
2026-01-06 19:18:24 +0600

    7.11: Redux, Step 2, the redux store can now get, create and remove data without the react state

--- part7/bloglist-frontend/src/store.js
+++ part7/bloglist-frontend/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notificationReducer from "./reducers/notificationSlice";
+import blogsReducer from './reducers/blogsSlice'
 
 export const store = configureStore({
     reducer: {
-        notification: notificationReducer
+        notification: notificationReducer,
+        blogs: blogsReducer
     }
 })
