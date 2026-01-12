271b337bd2bf810bdf4ab2da0faee28cc9668e2c
sush <m.hossain.sushmoy@gmail.com>
2026-01-12 19:51:21 +0600

    7.15: Individual User View; Implement a view for individual users that displays all of the blog posts added by that user; added a new userSlice for managing user data in the redux store then access the data in the user component to display the list of blogs created by the specific user

---
+++ part7/bloglist-frontend/src/reducers/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import userService from "../services/users";
+
+const initialState = [];
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    getUsers: (state, action) => action.payload,
+  },
+});
+
+export const { getUsers } = userSlice.actions;
+
+export const fetchUsers = () => async (dispatch) => {
+    const users = await userService.getAll()
+    dispatch(getUsers(users))
+}
+
+export default userSlice.reducer
