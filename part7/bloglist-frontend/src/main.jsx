8ee5afe1cb72f06d0bd9db329166114d046884d6
sush <m.hossain.sushmoy@gmail.com>
2026-01-12 13:04:52 +0600

    7.14: Users view; Implement a view to the application that displays all of the basic information related to users using react router in declarative mode

--- part7/bloglist-frontend/src/main.jsx
+++ part7/bloglist-frontend/src/main.jsx
@@ -3,9 +3,12 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
