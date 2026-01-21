f6c6f1e1d8e2d25765a7c4308c1e6319491a6d2b
Sushmoy <m.hossain.sushmoy@gmail.com>
2026-01-21 22:53:03 +0600

    8.2: All books; return all books by defining the type Book and query it

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -90,9 +90,18 @@ let books = [
 */
 
 const typeDefs = /* GraphQL */ `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
-    bookCount: Int
-    authorCount: Int
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -100,6 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
