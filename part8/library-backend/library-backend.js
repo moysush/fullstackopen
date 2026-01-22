c7eb8d4910c0619a4148a5415bee57397f19efe5
sush <m.hossain.sushmoy@gmail.com>
2026-01-23 02:09:37 +0600

    8.6: Adding a book; using Mutation and its arguments to add a book, also add its author if not already exist

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -97,7 +98,7 @@ const typeDefs = /* GraphQL */ `
   type Author {
     name: String!
     id: ID!
-    born: String
+    born: Int
     bookCount: Int
   }
 
@@ -106,6 +107,15 @@ const typeDefs = /* GraphQL */ `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
   }
 `;
 
@@ -129,6 +139,19 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter((b) => b.author === root.name).length;
+    },
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find((a) => a.name === args.author)) {
+        const author = { name: args.author, id: uuid() };
+        authors = authors.concat(author);
+      }
+
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      return book;
     },
   },
 };
