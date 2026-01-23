76c1267d0368b9dcbc839ec15c23ea11bfb16908
sush <m.hossain.sushmoy@gmail.com>
2026-01-23 23:14:02 +0600

    query with variables, useQuery with skip option

---
+++ part8/side-projects/graphql-phonebook-frontend/src/Components/Persons.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
+const Person = ({ person, onClose }) => {
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <div>
+        {person.address.street} {person.address.city}
+      </div>
+      <div>{person.phone}</div>
+      <button onClick={onClose}>close</button>
+    </div>
+  );
+};
+
+const Persons = ({ persons }) => {
+  const [nameToSearch, setNameToSearch] = useState(null);
+  const { data } = useQuery(FIND_PERSON, {
+    variables: { nameToSearch },
+    skip: !nameToSearch,
+  });
+
+  if (nameToSearch && data) {
+    return (
+      <Person person={data.findPerson} onClose={() => setNameToSearch(null)} />
+    );
+  }
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map((p) => (
+        <div key={p.id}>
+          {p.name} {p.phone}
+          <button onClick={() => setNameToSearch(p.name)}>show address</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Persons;
--- part8/side-projects/graphql-phonebook-frontend/src/main.jsx
+++ part8/side-projects/graphql-phonebook-frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { ApolloClient, gql, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
