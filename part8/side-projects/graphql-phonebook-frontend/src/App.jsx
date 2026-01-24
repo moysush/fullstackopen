aaa99bda804bbdbd43e4f5ae61d584d6d7b46b73
sush <m.hossain.sushmoy@gmail.com>
2026-01-25 00:01:31 +0600

    useMutation and updating cache with refetchQueries

--- part8/side-projects/graphql-phonebook-frontend/src/App.jsx
+++ part8/side-projects/graphql-phonebook-frontend/src/App.jsx
@@ -1,22 +1,15 @@
-import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 import Persons from "./Components/Persons";
+import PersonForm from "./Components/PersonForm";
+import { ALL_PERSONS } from "./queries";
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
 function App() {
   const { data, loading } = useQuery(ALL_PERSONS);
   if (loading) return "loading...";
   return (
     <>
       <Persons persons={data.allPersons} />
+      <PersonForm />
     </>
   );
 }
---
+++ part8/side-projects/graphql-phonebook-frontend/src/Components/PersonForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client/react";
+import { CREATE_PERSON, ALL_PERSONS } from "../queries";
+
+const PersonForm = () => {
+  const [form, setForm] = useState({
+    name: "",
+    street: "",
+    city: "",
+    phone: "",
+  });
+
+  const [savePerson] = useMutation(CREATE_PERSON, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault(); // if not set, it removes the initial values with the page refresh
+
+    savePerson({ variables: { ...form } });
+    setForm({
+      name: "",
+      street: "",
+      city: "",
+      phone: "",
+    });
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+        placeholder="name"
+      />
+      <input
+        name="street"
+        value={form.street}
+        onChange={handleChange}
+        placeholder="street"
+      />
+      <input
+        name="city"
+        value={form.city}
+        onChange={handleChange}
+        placeholder="city"
+      />
+      <input
+        name="phone"
+        value={form.phone}
+        onChange={handleChange}
+        placeholder="phone"
+      />
+      <button type="submit">add!</button>
+    </form>
+  );
+};
+
+export default PersonForm;
