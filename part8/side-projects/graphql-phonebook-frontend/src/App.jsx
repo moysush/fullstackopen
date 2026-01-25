e49bd30a4938305e0e7d48767d012a254a786517
sush <m.hossain.sushmoy@gmail.com>
2026-01-25 17:21:09 +0600

    settings error on client with onError for mutaion

--- part8/side-projects/graphql-phonebook-frontend/src/App.jsx
+++ part8/side-projects/graphql-phonebook-frontend/src/App.jsx
@@ -2,14 +2,27 @@ import { useQuery } from "@apollo/client/react";
 import Persons from "./Components/Persons";
 import PersonForm from "./Components/PersonForm";
 import { ALL_PERSONS } from "./queries";
+import Notify from "./Components/Notify";
+import { useState } from "react";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
   const { data, loading } = useQuery(ALL_PERSONS);
+
   if (loading) return "loading...";
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </>
   );
 }
