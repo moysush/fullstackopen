012225f87a7e9ea0cd715d266c8c7ef1fd0b9a91
sush <m.hossain.sushmoy@gmail.com>
2026-01-25 23:59:43 +0600

    8.8: Authors view

---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS } from "../query";
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null;
+  }
+  const { data, loading, error } = useQuery(ALL_AUTHORS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Born</th>
+            <th>Books</th>
+          </tr>
+          {data.allAuthors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
