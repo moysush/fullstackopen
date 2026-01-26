e7987abcfaa70e745b787ba77563891bbd61d779
Sushmoy <m.hossain.sushmoy@gmail.com>
2026-01-26 23:46:39 +0600

    initiate mongoDB server in phonebook-graphql-backend; add .env and refactor the codes

--- part8/side-projects/graphql-phonebook-backend/index.js
+++ part8/side-projects/graphql-phonebook-backend/index.js
@@ -1,125 +1,14 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const { GraphQLError } = require("graphql/error");
-const { v1: uuid } = require("uuid");
+require("dotenv").config();
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
-  },
-];
+const connectToDatabase = require("./db"); //db
+const startServer = require("./server"); // apollo server
 
-const typeDefs = /* GraphQL */ `
-  type Address {
-    street: String!
-    city: String!
-  }
+const MONGODB_URI = process.env.MONGODB_URI; //db
+const PORT = process.env.PORT || 4000; // apollo server
 
-  type Person {
-    name: String!
-    phone: String
-    address: Address!
-    id: ID!
-  }
-
-  enum YesNo {
-    YES
-    NO
-  }
-
-  type Query {
-    personCount: Int!
-    allPersons(phone: YesNo): [Person!]!
-    findPerson(name: String!): Person
-  }
-
-  type Mutation {
-    addPerson(
-      name: String!
-      phone: String
-      street: String!
-      city: String!
-    ): Person
-    editNumber(name: String!, phone: String!): Person
-  }
-`;
-
-const resolvers = {
-  Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
-      if (!args.phone) {
-        return persons;
-      }
-      const byPhone = (p) => (args.phone === "YES" ? p.phone : !p.phone);
-      return persons.filter(byPhone);
-    },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
-  },
-  // it's okay to omit it too if we don't need to hard code any default value
-  Person: {
-    name: (root) => root.name, // e.g. "defaultName"
-    phone: (root) => root.phone,
-    address: ({ street, city }) => {
-      return {
-        street,
-        city,
-      };
-    },
-    id: (root) => root.id,
-  },
-
-  Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new GraphQLError(`Name must be unique: ${args.name}`, {
-          extensions: {
-            code: "BAD_USER_INPUT",
-          },
-        });
-      }
-      const person = { ...args, id: uuid() };
-      persons = persons.concat(person);
-      return person;
-    },
-
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name);
-      if (!person) {
-        return null;
-      }
-
-      const updatedPerson = { ...person, phone: args.phone };
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
-      return updatedPerson;
-    },
-  },
+const main = async () => {
+  await connectToDatabase(MONGODB_URI); //db
+  startServer(PORT); // apollo server
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+main();
