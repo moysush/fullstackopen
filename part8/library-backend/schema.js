67e3ef386c3877b73bb11aab037c4d246caf50bf
sush <m.hossain.sushmoy@gmail.com>
2026-01-27 18:16:10 +0600

    8.13: Database, part 1; setup mongodb, refactor the codebase, store data in the db

---
+++ part8/library-backend/schema.js
@@ -0,0 +1,37 @@
+const typeDefs = /* GraphQL */ `
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+// the Author in Book object should include the whole Author details but the parameter of the addBook should only be sent with the author name
+module.exports = typeDefs;
