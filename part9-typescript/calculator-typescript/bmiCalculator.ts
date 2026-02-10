333210153ccb852c4a2b1af5e27e972f20fa9679
Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-11 00:30:19 +0600

    refactor bmiCalculator

--- part9-typescript/calculator-typescript/bmiCalculator.ts
+++ part9-typescript/calculator-typescript/bmiCalculator.ts
@@ -1,13 +1,11 @@
 const calculateBmi = (h: number, w: number): string => {
-  const bmi = w / (h / 100) ** 2
-  
-  if (bmi < 24.9 && bmi > 18.5) {
-    return "Normal range";
-  } else if (bmi > 24.9) {
-    return "Overweight range";
-  } else if (bmi < 18.5) {
-    return "Underweight range";
-  }
+  const bmi = w / (h / 100) ** 2;
+
+  return bmi < 18.5
+    ? "Underweight range"
+    : bmi < 25
+      ? "Normal range"
+      : "Overweight range";
 };
 
 console.log(calculateBmi(180, 74));
