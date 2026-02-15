1103334329221ead7244aaf30115a14b170b70e3
Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-15 23:12:16 +0600

    9.8: Patientor backend, step1; initiate a project for patientor backend, add eslint and tsconfig according to the course, add a ping req to /api/ping, add required scripts in the package.json, addtionally add ts-node-dev, typescript, tsc --init

---
+++ part9-typescript/patientor-backend/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/ping", (_req, res) => {
+  res.json("pong");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("server is running on port: ", PORT);
+});
