22b38bdc32e5af18da66e15dae4b141b22a3ad52
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-19 12:40:34 +0600

    Exercise 9.15; use typescript to re-create the course-info app; use interface types to properly type the props

---
+++ part9-typescript/courseinfo-ts/src/App.tsx
@@ -0,0 +1,70 @@
+interface NameProps {
+  name: string;
+}
+
+interface CoursePart extends NameProps {
+  exerciseCount: number;
+}
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+interface TotalProps {
+  totalExercises: number;
+}
+
+const Header = ({ name }: NameProps) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ courseParts }: ContentProps) => {
+  return (
+    <div>
+      {courseParts.map((part, index) => {
+        return (
+          <p key={part.name + index}>
+            {part.name} {part.exerciseCount}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+const Total = ({ totalExercises }: TotalProps) => {
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0,
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
