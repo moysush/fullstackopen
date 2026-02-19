14b782c47a2ccd48470ab31a4270b073db9db5c5
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-19 11:43:10 +0600

    initiate a react project with typescript

---
+++ part9-typescript/side-projects/react-with-types/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+interface WelcomeProps {
+  name: string
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+const Welcome = ({ name }: WelcomeProps) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Welcome name={"Sush"} />
+    <App />
+  </StrictMode>,
+);
