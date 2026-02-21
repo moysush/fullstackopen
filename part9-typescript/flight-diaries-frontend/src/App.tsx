1cdbb4c25df5d62e6090f7e2fd8b86fcb9013b57
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-21 16:35:06 +0600

    Exercise 9.18; make creating new diary possible from frontend

--- part9-typescript/flight-diaries-frontend/src/App.tsx
+++ part9-typescript/flight-diaries-frontend/src/App.tsx
@@ -8,22 +8,79 @@ function App() {
     weather: string;
     comment: string;
   }
+  const baseUrl = "http://localhost:3000/api/diaries";
   const [diaries, setDiaries] = useState<Diaries[]>([]);
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/diaries")
+    fetch(baseUrl)
       .then((res) => res.json() as Promise<Diaries[]>)
-      .then((data) => setDiaries(diaries.concat(data)));
+      .then((data) => setDiaries(diaries.concat(data)))
+      .catch((e) => console.log(e));
   }, []);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const newEntry = {
+      date,
+      visibility,
+      weather,
+      comment,
+    };
+
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newEntry),
+    })
+      .then((res) => res.json())
+      .then((data: Diaries) => setDiaries((prev) => prev.concat(data)));
+
+    // reset states
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
 
   return (
     <>
       <h2>Add New Entry</h2>
-      <form>
-        <input type="date" placeholder="Date" /> <br />
-        <input type="text" placeholder="Visibility" /> <br />
-        <input type="text" placeholder="Weather" /> <br />
-        <input type="text" placeholder="Comment" /> <br />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="date"
+          placeholder="Date"
+          value={date}
+          onChange={({ target }) => setDate(target.value)}
+        />{" "}
+        <br />
+        <input
+          type="text"
+          placeholder="Visibility"
+          value={visibility}
+          onChange={({ target }) => setVisibility(target.value)}
+        />{" "}
+        <br />
+        <input
+          type="text"
+          placeholder="Weather"
+          value={weather}
+          onChange={({ target }) => setWeather(target.value)}
+        />{" "}
+        <br />
+        <input
+          type="text"
+          placeholder="Comment"
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+        />{" "}
+        <br />
         <button type="submit">Add</button>
       </form>
       <h2>Diary Entries</h2>
