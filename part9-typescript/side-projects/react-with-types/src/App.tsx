a4ca70bf1f3c4cc48ca19c00a91b296e3e515757
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-21 11:57:53 +0600

    React app with typed states

--- part9-typescript/side-projects/react-with-types/src/App.tsx
+++ part9-typescript/side-projects/react-with-types/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Note {
+    id: string;
+    content: string;
+  }
+  const [notes, setNotes] = useState<Note[]>([{ id: "1", content: "testing" }]);
+  const [newNote, setNewNote] = useState("");
+  
+  const noteCreation = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const noteToAdd = {
+      id: String(notes.length + 1),
+      content: newNote,
+    }
+
+    setNotes(notes.concat(noteToAdd))
+    setNewNote('')
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={({ target }) => setNewNote(target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
