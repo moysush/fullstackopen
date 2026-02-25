3ccb5f48a0f4c29a2303fc27ee0e48e34f9389ae
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-25 15:30:36 +0600

    type the every type of entries

--- part9-typescript/patientor-backend/src/types.ts
+++ part9-typescript/patientor-backend/src/types.ts
@@ -1,7 +1,51 @@
 import z from "zod";
 import { NewPatientSchema } from "./utils";
 
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Diagnosis["code"][]; // array of codes
+}
+
+export enum healthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: healthCheckRating;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
 
 export interface Diagnosis {
   code: string;
