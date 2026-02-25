d0a08f91489b8ce359e7eab0cdff7531ef237c6c
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-25 20:06:31 +0600

    Omit with unions

--- part9-typescript/patientor-backend/src/types.ts
+++ part9-typescript/patientor-backend/src/types.ts
@@ -9,34 +9,34 @@ interface BaseEntry {
   diagnosisCodes?: Diagnosis["code"][]; // array of codes
 }
 
-export enum healthCheckRating {
+enum healthCheckRating {
   "Healthy" = 0,
   "LowRisk" = 1,
   "HighRisk" = 2,
   "CriticalRisk" = 3,
 }
 
-export interface HealthCheckEntry extends BaseEntry {
+interface HealthCheckEntry extends BaseEntry {
   type: "HealthCheck";
   healthCheckRating: healthCheckRating;
 }
 
-export interface Discharge {
+interface Discharge {
   date: string;
   criteria: string;
 }
 
-export interface HospitalEntry extends BaseEntry {
+interface HospitalEntry extends BaseEntry {
   type: "Hospital";
   discharge: Discharge;
 }
 
-export interface SickLeave {
+interface SickLeave {
   startDate: string;
   endDate: string;
 }
 
-export interface OccupationalHealthcareEntry extends BaseEntry {
+interface OccupationalHealthcareEntry extends BaseEntry {
   type: "OccupationalHealthcare";
   employerName: string;
   sickLeave?: SickLeave;
@@ -46,6 +46,10 @@ export type Entry =
   | HealthCheckEntry
   | HospitalEntry
   | OccupationalHealthcareEntry;
+
+ type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
 
 export interface Diagnosis {
   code: string;
@@ -71,3 +75,4 @@ export enum Gender {
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
+export type EntryWithoutId = UnionOmit<Entry, "id">;
