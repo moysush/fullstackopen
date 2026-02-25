9e3bb417cc643f587ffecff2dc975bedb1e3a25f
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-02-26 04:13:31 +0600

    9.26: Patientor, step 6; use switch statement to include different type of date for different type for the Entry, use MUI icons to show type and health rating. Additionally extract the EntryDetails components into its own file and use exhausting type never. Properly utilized MUI for clean and modern UI.

--- part9-typescript/patientor-frontend/src/components/PatientListPage/PatientDetails.tsx
+++ part9-typescript/patientor-frontend/src/components/PatientListPage/PatientDetails.tsx
@@ -3,15 +3,17 @@ import { Diagnosis, Patient } from "../../types";
 import patientService from "../../services/patients";
 import diagnosesService from "../../services/diagnoses";
 import { useEffect, useState } from "react";
-import { Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import TransgenderIcon from "@mui/icons-material/Transgender";
+import EntryDetails from "./EntryDetails";
 
 const PatientDetails = () => {
   const { id } = useParams<string>();
-  const [patient, setPatient] = useState<Patient | undefined>();
-  const [diagnoses, setDiagnoses] = useState<Diagnosis[] | undefined>();
+  // even if i don't use type | undefined, typescript auto assigns undefined if the initial value is empty
+  const [patient, setPatient] = useState<Patient>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
   useEffect(() => {
     // narrows the id type to string if it exists
@@ -25,6 +27,7 @@ const PatientDetails = () => {
   if (!patient) {
     return <Typography>Patient not found...</Typography>;
   }
+  // narrows the diagnoses type excluding undefined
   if (!diagnoses) {
     return null;
   }
@@ -52,31 +55,24 @@ const PatientDetails = () => {
         <strong>Occupation: </strong>
         {patient.occupation}
       </Typography>
-      <Typography variant="h6" sx={{ mt: 2 }}>
-        <strong>Entries</strong>
-      </Typography>
-      {patient.entries.length === 0
-        ? "No entries yet..."
-        : patient.entries.map((e) => {
-            return (
-              <div>
-                <Typography>{e.date}</Typography>
-                <Typography>
-                  <i>{e.description}</i>
-                </Typography>
-                <ul>
-                  {e.diagnosisCodes?.map((c) => {
-                    return (
-                      <li>
-                        {c}{" "}
-                        {diagnoses?.map((d) => (d.code === c ? d.name : null))}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            );
-          })}
+      <div id="Entries">
+        <Typography variant="h6" sx={{ mt: 2 }}>
+          <strong>Entries</strong>
+        </Typography>
+        {patient.entries.length === 0
+          ? "No entries yet..."
+          : patient.entries.map((e) => {
+              return (
+                <Paper
+                  key={e.id}
+                  sx={{ p: 2, mt: 2, "&:hover": { boxShadow: 8 } }}
+                  elevation={3}
+                >
+                  <EntryDetails entry={e} diagnoses={diagnoses} />
+                </Paper>
+              );
+            })}
+      </div>
     </div>
   );
 };
